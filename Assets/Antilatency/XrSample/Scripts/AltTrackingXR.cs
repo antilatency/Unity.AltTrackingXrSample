ea8a8fa1dc29fe27a115603edb0de5a3b57de3ac
Nikita Psarev <psarev@github.com>
2021-10-01 19:31:28 +0300

    Update SDK version to 3.3.0

--- Assets/Antilatency/XrSample/Scripts/AltTrackingXR.cs
+++ Assets/Antilatency/XrSample/Scripts/AltTrackingXR.cs
@@ -1,14 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using UnityEngine;
 
-using UnityEngine;
-
-using Antilatency;
-using Antilatency.Integration;
+using Antilatency.SDK;
 using Antilatency.DeviceNetwork;
 
-public class AltTrackingXR : AltTracking {
+public class AltTrackingXR : AltTracking
+{
     public Camera XRCamera;
     public UnityEngine.SpatialTracking.TrackedPoseDriver HmdPoseDriver;
 
@@ -25,25 +21,31 @@ public class AltTrackingXR : AltTracking {
     private Transform _bSpace;
     private Transform _b;
 
-    protected override NodeHandle GetAvailableTrackingNode() {
+    protected override NodeHandle GetAvailableTrackingNode()
+    {
         return GetUsbConnectedFirstIdleTrackerNode();
     }
 
-    protected override Pose GetPlacement() {
+    protected override Pose GetPlacement()
+    {
         var result = Pose.identity;
 
-        using (var localStorage = Antilatency.Integration.StorageClient.GetLocalStorage()) {
-
-            if (localStorage == null) {
+        using (var localStorage = Antilatency.SDK.StorageClient.GetLocalStorage())
+        {
+            if (localStorage == null)
+            {
                 return result;
             }
 
             var placementCode = localStorage.read("placement", "default");
 
-            if (string.IsNullOrEmpty(placementCode)) {
+            if (string.IsNullOrEmpty(placementCode))
+            {
                 Debug.LogError("Failed to get placement code");
                 result = Pose.identity;
-            } else {
+            }
+            else
+            {
                 result = _trackingLibrary.createPlacement(placementCode);
             }
 
@@ -51,21 +53,72 @@ public class AltTrackingXR : AltTracking {
         }
     }
 
-    protected override void Awake() {
+    protected virtual void OnFocusChanged(bool focus)
+    {
+        if (focus)
+        {
+            StartTrackingAlignment();
+        }
+        else
+        {
+            StopTrackingAlignment();
+        }
+    }
+
+    private void StartTrackingAlignment()
+    {
+        if (_alignment != null)
+        {
+            StopTrackingAlignment();
+        }
+
+        var placement = GetPlacement();
+        _alignment = _alignmentLibrary.createTrackingAlignment(placement.rotation, ExtrapolationTime);
+
+        _altInitialPositionApplied = false;
+    }
+
+    private void StopTrackingAlignment()
+    {
+        if (_alignment == null)
+        {
+            return;
+        }
+
+        _alignment.Dispose();
+        _alignment = null;
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        OnFocusChanged(focus);
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+        OnFocusChanged(!pause);
+    }
+
+    protected override void Awake()
+    {
         base.Awake();
 
         _alignmentLibrary = Antilatency.TrackingAlignment.Library.load();
 
         var placement = GetPlacement();
-        _alignment = _alignmentLibrary.createTrackingAlignment(Antilatency.Math.doubleQ.FromQuaternion(placement.rotation), ExtrapolationTime);
+        _alignment = _alignmentLibrary.createTrackingAlignment(placement.rotation, ExtrapolationTime);
 
-        if (XRCamera == null) {
+        if (XRCamera == null)
+        {
             XRCamera = GetComponentInChildren<Camera>();
-            if (XRCamera == null) {
+            if (XRCamera == null)
+            {
                 Debug.LogError("XR Camera is not setted and no cameras has been found in children gameobjects");
                 enabled = false;
                 return;
-            } else {
+            }
+            else
+            {
                 Debug.LogWarning("XR Camera: " + XRCamera.gameObject.name);
             }
         }
@@ -81,22 +134,26 @@ public class AltTrackingXR : AltTracking {
         _b = XRCamera.transform;
     }
 
-    protected override void Update() {
+    protected override void Update()
+    {
         base.Update();
 
         var centerEye = UnityEngine.XR.InputDevices.GetDeviceAtXRNode(UnityEngine.XR.XRNode.CenterEye);
 
-        if (!centerEye.isValid) {
+        if (!centerEye.isValid)
+        {
             Debug.LogWarning("Center eye device is not valid");
             return;
         }
 
         // Do nothing if HMD is not putted on because some devices send invalid tracking data in such state.
-        if (!centerEye.TryGetFeatureValue(UnityEngine.XR.CommonUsages.userPresence, out var userPresence) || !userPresence) {
+        if (!centerEye.TryGetFeatureValue(UnityEngine.XR.CommonUsages.userPresence, out var userPresence) || !userPresence)
+        {
             return;
         }
 
-        if (!centerEye.TryGetFeatureValue(UnityEngine.XR.CommonUsages.trackingState, out var state)) {
+        if (!centerEye.TryGetFeatureValue(UnityEngine.XR.CommonUsages.trackingState, out var state))
+        {
             return;
         }
 
@@ -106,58 +163,70 @@ public class AltTrackingXR : AltTracking {
         bool altTrackingActive;
         Antilatency.Alt.Tracking.State trackingState;
 
-		altTrackingActive = GetRawTrackingState(out trackingState);
+        altTrackingActive = GetRawTrackingState(out trackingState);
 
         // If Alt is disconnected, we have nothing to do.
-        if (!altTrackingActive) {
+        if (!altTrackingActive)
+        {
             return;
         }
 
-		if (_lerpRotation && trackingState.stability.stage == Antilatency.Alt.Tracking.Stage.Tracking6Dof && bRotationRecieved) {
-			var result = _alignment.update(
-				Antilatency.Math.doubleQ.FromQuaternion(trackingState.pose.rotation), 
-				Antilatency.Math.doubleQ.FromQuaternion(bRotation), 
-				Time.realtimeSinceStartup);
+        if (_lerpRotation && (_alignment != null) && trackingState.stability.stage == Antilatency.Alt.Tracking.Stage.Tracking6Dof && bRotationRecieved)
+        {
+            var result = _alignment.update(
+                trackingState.pose.rotation,
+                bRotation,
+                Time.realtimeSinceStartup);
 
-			ExtrapolationTime = (float)result.timeBAheadOfA;
-			_placement.rotation = result.rotationARelativeToB.ToQuaternion();
-			_bSpace.localRotation = result.rotationBSpace.ToQuaternion();
-		}
+            ExtrapolationTime = result.timeBAheadOfA;
+            _placement.rotation = result.rotationARelativeToB;
+            _bSpace.localRotation = result.rotationBSpace;
+        }
 
         altTrackingActive = GetTrackingState(out trackingState);
-        if (!altTrackingActive || trackingState.stability.stage == Antilatency.Alt.Tracking.Stage.InertialDataInitialization) {
+        if (!altTrackingActive || trackingState.stability.stage == Antilatency.Alt.Tracking.Stage.InertialDataInitialization)
+        {
             return;
         }
 
-        if (!_lerpRotation) {
+        if (!_lerpRotation)
+        {
             _bSpace.localRotation = trackingState.pose.rotation;
             _b.localRotation = Quaternion.identity;
         }
-            
-        if (_lerpPosition) {
-            if (trackingState.stability.stage == Antilatency.Alt.Tracking.Stage.Tracking6Dof && bPositionRecieved) {
+
+        if (_lerpPosition)
+        {
+            if (trackingState.stability.stage == Antilatency.Alt.Tracking.Stage.Tracking6Dof && bPositionRecieved)
+            {
                 var a = trackingState.pose.position;
                 var bSpace = _bSpace.localPosition;
                 var b = _aSpace.InverseTransformPoint(_bSpace.TransformPoint(bPosition));
 
                 Vector3 averagePositionInASpace;
 
-                if (!_altInitialPositionApplied) {
+                if (!_altInitialPositionApplied)
+                {
                     averagePositionInASpace = a;
                     _altInitialPositionApplied = true;
-                } else {
+                }
+                else
+                {
                     averagePositionInASpace = (b * _bQuality + a * trackingState.stability.value) / (trackingState.stability.value + _bQuality);
                 }
 
                 _bSpace.localPosition += averagePositionInASpace - b;
             }
-        } else {
+        }
+        else
+        {
             _bSpace.localPosition = trackingState.pose.position;
             _b.localPosition = Vector3.zero;
         }
     }
 
-    private float Fx(float x, float k) {
+    private float Fx(float x, float k)
+    {
         var xDk = x / k;
         return 1.0f / (xDk * xDk + 1);
     }
